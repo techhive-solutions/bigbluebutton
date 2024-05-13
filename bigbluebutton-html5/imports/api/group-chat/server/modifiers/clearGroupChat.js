9938e4ab01a6ca59409d0bac8d714606f96978b7
mbm_1607 <muhammadkhan1607@gmail.com>
2024-05-13 13:27:44 +0500

    :sparkles: Transfer group chat before clearing it
    Add new server method `transferGroupChat`, this is used to transfer the
    public & private chat messages to TutorBees.net before clearing the
    group chat. This allows admin to review these sensitive messages if
    needed.

--- bigbluebutton-html5/imports/api/group-chat/server/modifiers/clearGroupChat.js
+++ bigbluebutton-html5/imports/api/group-chat/server/modifiers/clearGroupChat.js
@@ -1,9 +1,14 @@
-import GroupChat from '/imports/api/group-chat';
-import Logger from '/imports/startup/server/logger';
-import clearGroupChatMsg from '/imports/api/group-chat-msg/server/modifiers/clearGroupChatMsg';
+import GroupChat from "/imports/api/group-chat";
+import clearGroupChatMsg from "/imports/api/group-chat-msg/server/modifiers/clearGroupChatMsg";
+import transferGroupChat from "/imports/api/pads/server/methods/transferGroupChat";
+import Logger from "/imports/startup/server/logger";
 
 export default async function clearGroupChat(meetingId) {
   try {
+    // ? Custom Change: Before clearing the GroupChat, transfer the GroupChat to external service
+    // This is to ensure that the chat data is not lost & can be utilized for future references
+    await transferGroupChat(meetingId);
+
     await clearGroupChatMsg(meetingId);
     const numberAffected = await GroupChat.removeAsync({ meetingId });
 
