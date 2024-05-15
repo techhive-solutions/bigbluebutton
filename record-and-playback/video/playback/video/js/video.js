99da94295448bb491fc4d3ae4d8c917f75c3e01f
mbm_1607 <muhammadkhan1607@gmail.com>
2024-05-15 20:28:18 +0500

    :sparkle: Add download button on video playback

--- record-and-playback/video/playback/video/js/video.js
+++ record-and-playback/video/playback/video/js/video.js
@@ -1,13 +1,23 @@
 function parseParams() {
   var map = {};
-  window.location.search.replace(/[?&#]+([^=&]+)=([^&]*)/gi, function (m, key, value) { map[key] = value; });
-  window.location.hash.replace(/[?&#]+([^=&]+)=([^&]*)/gi, function (m, key, value) { map[key] = value; });
+  window.location.search.replace(
+    /[?&#]+([^=&]+)=([^&]*)/gi,
+    function (m, key, value) {
+      map[key] = value;
+    }
+  );
+  window.location.hash.replace(
+    /[?&#]+([^=&]+)=([^&]*)/gi,
+    function (m, key, value) {
+      map[key] = value;
+    }
+  );
   return map;
 }
 
 function parseStartTime() {
   var params = parseParams();
-  if (typeof(params.t) === "undefined") {
+  if (typeof params.t === "undefined") {
     return 0;
   }
 
@@ -18,59 +28,77 @@ function parseStartTime() {
   while (true) {
     var value = extractNumber.exec(params.t);
     var unit = extractUnit.exec(params.t);
-    if (value == null || unit == null)
-      break;
+    if (value == null || unit == null) break;
 
     unit = String(unit).toLowerCase();
     value = parseInt(String(value), 10);
 
-    if (unit == "h")
-      value *= 3600;
-    else if (unit == "m")
-      value *= 60;
+    if (unit == "h") value *= 3600;
+    else if (unit == "m") value *= 60;
 
     startTime += value;
   }
   return startTime;
 }
 
-Popcorn(function() {
+Popcorn(function () {
   var vjs = videojs("video", {
     techOrder: ["html5"],
-    playbackRates: [1, 1.25, 1.5, 2]
+    playbackRates: [1, 1.25, 1.5, 2],
   });
 
-  vjs.ready(function() {
+  vjs.ready(function () {
     var popcorn = Popcorn("#video_html5_api", {
       defaults: {
         chattimeline: {
-          target: "chat"
-        }
-      }
+          target: "chat",
+        },
+      },
     });
 
     var t = parseStartTime();
-    if (t > 0)
-      popcorn.currentTime(t);
+    if (t > 0) popcorn.currentTime(t);
 
-    window.addEventListener("hashchange", function() {
-      var new_t = parseStartTime();
-      if (new_t != t) {
-        t = new_t;
-        popcorn.currentTime(t);
-      }
-    }, false);
+    window.addEventListener(
+      "hashchange",
+      function () {
+        var new_t = parseStartTime();
+        if (new_t != t) {
+          t = new_t;
+          popcorn.currentTime(t);
+        }
+      },
+      false
+    );
 
-    if (navigator.userAgent.indexOf('Firefox') != -1) {
-      var sources = popcorn.media.querySelectorAll('source')
+    if (navigator.userAgent.indexOf("Firefox") != -1) {
+      var sources = popcorn.media.querySelectorAll("source");
       if (sources.length > 0) {
-        var lastSource = sources[sources.length-1];
-        lastSource.addEventListener('error', function(ev) {
-          document.getElementById('firefox-format-warning').style.display = 'block';
-        }, false);
+        var lastSource = sources[sources.length - 1];
+        lastSource.addEventListener(
+          "error",
+          function (ev) {
+            document.getElementById("firefox-format-warning").style.display =
+              "block";
+          },
+          false
+        );
       }
     }
 
     popcorn.parseXML("video.xml");
+  });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  const video = document.getElementById("video");
+  const h1 = document.getElementById("h1");
+  document.addEventListener("button", function () {
+    const a = document.getElementById("a");
+    a.href = video.src;
+    a.download = `recording-${h1.innerText}.mv4`;
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
   });
 });
